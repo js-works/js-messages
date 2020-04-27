2064191186a9c0b2997918baf271401153aad81c
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2020-04-27 23:43:29 +0200

    Some type updates

--- src/main/api/defineMessages.ts
+++ src/main/api/defineMessages.ts
@@ -6,31 +6,19 @@ import buildMessageCreator from '../internal/buildMessageCreator';
 
 type Func<A extends any[], R> = (...args: A) => R
 
-type Arguments<A extends any[], I extends MessageInitializer<any>>
-  = I extends Func<A, any>
-    ? Signature<I>
-    : I extends { payload: Func<A, any> }
-      ? Signature<I['payload']>
-      : I extends { meta: Func<A, any> }
-        ? Signature<I['meta']>
-        : []
-
-type Payload<I extends MessageInitializer<any>>
-  = I extends Func<any, any>
-    ? ReturnType<I> 
-    : (I extends { payload: Func<any, any> }
-      ? ReturnType<I['payload']>
-      : any) 
-
-type Meta<I extends MessageInitializer<any>>
-  = I extends Func<any, any>
-    ? ReturnType<I> 
-    : (I extends { meta: Func<any, any> }
-      ? ReturnType<I['meta']>
-      : any) 
+type MessageCreatorType<T, X extends MessageInitializer<any>> =
+  X extends Func<infer A, infer P>
+    ? (...args: A) => { type: T, payload: P }
+    : X extends { payload: Func<infer A, infer P>, meta: Func<infer A, infer M> }
+      ? (...args: A) => { type: T, payload: P, meta: M }
+      : X extends { payload: Func<infer A, infer P> }
+        ? (...args: A) => { type: T, payload: P }
+        : X extends { meta: Func<infer A, infer M> }
+          ? (...args: A) => { type: T, meta: M }
+          : () => { type: T }
 
 function defineMessages<C extends MessagesConfig>(config: C):
-  { [T in keyof C]: MessageCreator<T, Payload<C[T]>, Meta<C[T]>, Arguments<any, C[T]>> }  {
+  { [T in keyof C]: MessageCreatorType<T, C[T]> }  {
 
   const
     ret: any = {},
