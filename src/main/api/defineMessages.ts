bd0247af044a8c19fca5d318a1fcccd185a6093d
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2020-04-28 00:30:05 +0200

    Some updates

--- src/main/api/defineMessages.ts
+++ src/main/api/defineMessages.ts
@@ -1,15 +1,5 @@
 type Func<A extends any[], R> = (...args: A) => R
 
-type MessageCreator<T extends string, P, M, A extends any[]> = {
-  readonly type: T,
-  
-  (...args: A): {
-    readonly type: T,
-    readonly payload?: P,
-    readonly meta?: M
-  }
-}
-
 type MessageInitializer<A extends any[]> =
   Func<A, any>
     | { payload?: Func<A, any>, meta?:Func<A, any> }
@@ -20,7 +10,7 @@ type MessagesConfig = {
       | { payload?: Func<any, any>, meta?: Func<any, any> } 
 }
 
-type MessageCreatorType<T /* extends string */, X extends MessageInitializer<any>> =
+type MessageCreatorType<T, X> =
   X extends Func<infer A, infer P>
     ? { (...args: A): { type: T, payload: P }, type: T }
     : X extends { payload: Func<infer A, infer P>, meta: Func<infer A, infer M> }
@@ -36,55 +26,44 @@ function defineMessages<C extends MessagesConfig>(config: C):
 
   const
     ret: any = {},
-    keys = Object.keys(config)
+    types = Object.keys(config)
 
-  for (let i = 0; i < keys.length; ++i) {
-    const key = keys[i]
+  for (let i = 0; i < types.length; ++i) {
+    const
+      type = types[i],
+      initializer = config[type]
 
-    ret[key] = buildMessageCreator(key, config[key])
-  }
+    if (typeof  initializer === 'function') {
+      const getPayload = initializer as Func<any, any>
+      
+      ret[type] = function (/* arguments */) {
+        const payload = getPayload.apply(null, arguments as any)
 
-  return ret
-}
-
-function buildMessageCreator<T extends string, A extends any[]>
-  (type: T, initializer: MessageInitializer<A> | null = null): MessageCreator<T, any, any, A> {
-  
-  let ret: any
-
-  if (!initializer) {
-   const msg = Object.freeze({ type })
-
-    ret = () => msg
-  } else if (typeof initializer === 'function') {
-    const getPayload = initializer as Function
-    
-    ret = function (/* arguments */) {
-      const payload = getPayload.apply(null, arguments)
-
-      return { type, payload }
-    }
-  } else {
-    const { payload: getPayload, meta: getMeta } = initializer as { payload?: Function, meta?: Function }
-
-    ret = function (/* arguments */) {
-      const msg: any = { type: type }
-
-      if (getPayload) {
-        msg.payload = getPayload.apply(null, arguments)
+        return { type, payload }
       }
+    } else {
+      const { payload: getPayload, meta: getMeta } =
+        initializer as { payload?: Func<any, any>, meta?: Func<any, any> }
 
-      if (getMeta) {
-        msg.meta = getMeta.apply(null, arguments)
+      ret[type] = function (/* arguments */) {
+        const msg: any = { type: type }
+
+        if (getPayload) {
+          msg.payload = getPayload.apply(null, arguments as any)
+        }
+
+        if (getMeta) {
+          msg.meta = getMeta.apply(null, arguments as any)
+        }
+
+        return msg
       }
-
-      return msg
     }
-  }
 
-  Object.defineProperty(ret, 'type', {
-    value: type
-  })
+    Object.defineProperty(ret[type], 'type', {
+      value: type
+    })
+  }
 
   return ret
 }
