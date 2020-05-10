95b5527e20547f77efc3a41dc4350420142aaac1
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2020-05-10 15:58:18 +0200

    Some API enhancements

--- src/main/api/defineMessages.ts
+++ src/main/api/defineMessages.ts
@@ -2,30 +2,20 @@ import defineMessage from './defineMessage'
 import Func from '../internal/Func'
 import MessageCreatorType from '../internal/MessageCreatorType'
 
-type MessageInitializer1<A extends any[]> =
+type MessageInitializer<A extends any[]> =
   Func<A, any>
-    | { payload?: Func<A, any>, meta?:Func<A, any> }
+    | { type?: string, payload?: Func<A, any>, meta?:Func<A, any> }
 
-type MessagesConfig1 = {
-  [type: string]: MessageInitializer1<any[]>
+type MessagesConfig = {
+  [type: string]: MessageInitializer<any[]>
 }
 
-type MessageInitializer2<T extends string, A extends any[]> =
-    { type: T, payload?: Func<A, any>, meta?:Func<A, any> }
-
-type MessagesConfig2 = {
-  [key: string]: MessageInitializer2<string, any>
-}
-
-function defineMessages<C extends MessagesConfig1>(config: C):
-  { [T in keyof C]: MessageCreatorType<T, C[T]> }
-
-function defineMessages<C extends MessagesConfig2>(config: C):
-  { [K in keyof C]: MessageCreatorType<C[K]['type'], Omit<C[K], 'type'>> }
-
-function defineMessages<C extends MessagesConfig1>(config: C):
-  { [key: string]: MessageCreatorType<string, any> } {
-
+function defineMessages<C extends MessagesConfig>(config: C): {
+  [K in keyof C]:
+    C[K] extends { type: infer T }
+      ? MessageCreatorType<T, Omit<C[K], 'type'>>
+      : MessageCreatorType<K, C[K]>
+} {
   const
     ret: any = {},
     keys = Object.keys(config)
