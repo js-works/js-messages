ece2230a6f794a093ad009632ef3b6a5cd628bfd
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2020-05-10 15:45:10 +0200

    API is now a bit more flexible

--- src/main/api/defineMessages.ts
+++ src/main/api/defineMessages.ts
@@ -2,28 +2,47 @@ import defineMessage from './defineMessage'
 import Func from '../internal/Func'
 import MessageCreatorType from '../internal/MessageCreatorType'
 
-type MessageInitializer<A extends any[]> =
+type MessageInitializer1<A extends any[]> =
   Func<A, any>
     | { payload?: Func<A, any>, meta?:Func<A, any> }
 
-type MessagesConfig = {
-  [type: string]: MessageInitializer<any[]>
+type MessagesConfig1 = {
+  [type: string]: MessageInitializer1<any[]>
 }
 
-function defineMessages<C extends MessagesConfig>(config: C):
-  { [T in keyof C]: MessageCreatorType<T, C[T]> } {
+type MessageInitializer2<T extends string, A extends any[]> =
+    { type: T, payload?: Func<A, any>, meta?:Func<A, any> }
+
+type MessagesConfig2 = {
+  [key: string]: MessageInitializer2<string, any>
+}
+
+function defineMessages<C extends MessagesConfig1>(config: C):
+  { [T in keyof C]: MessageCreatorType<T, C[T]> }
+
+function defineMessages<C extends MessagesConfig2>(config: C):
+  { [K in keyof C]: MessageCreatorType<C[K]['type'], Omit<C[K], 'type'>> }
+
+function defineMessages<C extends MessagesConfig1>(config: C):
+  { [key: string]: MessageCreatorType<string, any> } {
 
   const
     ret: any = {},
-    types = Object.keys(config)
+    keys = Object.keys(config)
 
-  for (let i = 0; i < types.length; ++i) {
-    const
-      type = types[i],
-      initializer = config[type]
-    
-    ret[type] = defineMessage(type, initializer)
-  }
+  keys.forEach(key => {
+    let
+      type =  key,
+      initializer: any = config[key]
+
+    if (initializer && typeof initializer === 'object' && typeof initializer.type === 'string') {
+      type = initializer.type
+      initializer = { ...initializer }
+      delete initializer.type
+    }
+
+    ret[key] = defineMessage(type, initializer)
+  })
 
   return ret
 }
--- src/test/api/defineMessages.test.ts
+++ src/test/api/defineMessages.test.ts
@@ -21,6 +21,33 @@ const Actions = defineMessages({
   }
 })
 
+const Actions2 = defineMessages({
+  action1: {
+    type: 'category.action1'
+  },
+
+  action2: {
+    type: 'category.action2',
+    payload: (value: number = 0) => value
+  },
+
+  action3: {
+    type: 'category.action3',
+    payload: (value: number = 21) => ({ value })
+  },
+
+  action4: {
+    type: 'category.action4',
+    payload: (value: number = 42) => ({ value }),
+    meta: (value: number = 42) => ({ half: value / 2})
+  },
+
+  action5: {
+    type: 'category.action5',
+    meta: (value: number = 42) => ({ half: value / 2})
+  }
+} as const)
+
 describe('defineMessages', () => {
   it('should create messages with neither payload nor meta', () => {
     expect(Actions.action1())
@@ -48,5 +75,35 @@ describe('defineMessages', () => {
     
     expect(Actions.action5(42))
       .to.eql({ type: 'action5', meta: { half: 21 } })
+  })
+})
+
+describe('defineMessages (alternative syntax)', () => {
+  it('should create messages with neither payload nor meta', () => {
+    expect(Actions2.action1())
+      .to.eql({ type: 'category.action1' })
+  })
+
+  it('should create messages properly with payload getter', () => {
+    expect(Actions2.action2())
+      .to.eql({ type: 'category.action2', payload: 0 })
+
+    expect(Actions2.action2(2))
+      .to.eql({ type: 'category.action2', payload: 2 })
+   
+    expect(Actions2.action3())
+      .to.eql({ type: 'category.action3', payload: { value: 21 } })
+
+    expect(Actions2.action3(2))
+      .to.eql({ type: 'category.action3', payload: { value: 2 } })
+   
+    expect(Actions2.action4())
+      .to.eql({ type: 'category.action4', payload: { value: 42 }, meta: { half: 21 } })
+
+    expect(Actions2.action4(2))
+      .to.eql({ type: 'category.action4', payload: { value: 2 }, meta: { half: 1 } })
+    
+    expect(Actions2.action5(42))
+      .to.eql({ type: 'category.action5', meta: { half: 21 } })
   })
 })
