41e084f9321a5033a25b1ee6342af0b1357a32c6
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2020-07-18 20:22:59 +0200

    Added function override defineMessages(category, config)

--- src/main/api/defineMessages.ts
+++ src/main/api/defineMessages.ts
@@ -7,7 +7,7 @@ type MessagesConfig = {
   [type: string]: null | ((...args: any[]) => Props)
 }
 
-type MessageCreatorsOf<C extends MessagesConfig> = {
+type MessageCreators1Of<C extends MessagesConfig> = {
   [T in keyof C]: T extends string
     ? C[T] extends (...args: infer A) => infer P
       ? MessageCreator<T, A, P>
@@ -15,9 +15,30 @@ type MessageCreatorsOf<C extends MessagesConfig> = {
     : never
 }
 
-function defineMessages<T extends string, C extends MessagesConfig>(
+type MessageCreators2Of<C extends MessagesConfig> = {
+  [T in keyof C]: T extends string
+    ? C[T] extends (...args: infer A) => infer P
+      ? MessageCreator<string, A, P>
+      : MessageCreator<string>
+    : never
+}
+
+function defineMessages<C extends MessagesConfig>(
   config: C
-): MessageCreatorsOf<C> {
+): MessageCreators1Of<C>
+
+function defineMessages<C extends MessagesConfig>(
+  namespace: string,
+  config: C
+): MessageCreators2Of<C>
+
+function defineMessages(arg1: any, arg2?: any): any {
+  return arg2 ? defineMessages2(arg1, arg2) : defineMessages1(arg1)
+}
+
+function defineMessages1<C extends MessagesConfig>(
+  config: C
+): MessageCreators2Of<C> {
   const ret: any = {}
   const keys = Object.keys(config)
 
@@ -36,6 +57,33 @@ function defineMessages<T extends string, C extends MessagesConfig>(
     }
 
     ret[key] = defineMessage(type, initializer)
+  })
+
+  return ret
+}
+
+function defineMessages2<C extends MessagesConfig>(
+  category: string,
+  config: C
+): MessageCreators2Of<C> {
+  const ret: any = {}
+  const keys = Object.keys(config)
+
+  keys.forEach((key) => {
+    let type = key
+    let initializer: any = config[key]
+
+    if (
+      initializer &&
+      typeof initializer === 'object' &&
+      typeof initializer.type === 'string'
+    ) {
+      type = initializer.type
+      initializer = { ...initializer }
+      delete initializer.type
+    }
+
+    ret[key] = defineMessage(`${category}.${type}`, initializer)
   })
 
   return ret
--- src/test/api/defineMessages.test.ts
+++ src/test/api/defineMessages.test.ts
@@ -8,13 +8,37 @@ const Actions = defineMessages({
   action2: (value: number = 0) => ({ value, flag: true })
 })
 
+const Actions2 = defineMessages('category', {
+  action1: null,
+  action2: (value: number = 0) => ({ value, flag: true })
+})
+
 describe('defineMessages', () => {
-  it('should create messages with out props', () => {
+  it('should create messages without props', () => {
     expect(Actions.action1()).to.eql({ type: 'action1' })
   })
 
   it('should create messages properly with props', () => {
     expect(Actions.action2()).to.eql({ type: 'action2', value: 0, flag: true })
     expect(Actions.action2(2)).to.eql({ type: 'action2', value: 2, flag: true })
+  })
+})
+
+describe('defineMessages with category', () => {
+  it('should create categorized messages without props', () => {
+    expect(Actions2.action1()).to.eql({ type: 'category.action1' })
+  })
+
+  it('should create categorized messages properly with props', () => {
+    expect(Actions2.action2()).to.eql({
+      type: 'category.action2',
+      value: 0,
+      flag: true
+    })
+    expect(Actions2.action2(2)).to.eql({
+      type: 'category.action2',
+      value: 2,
+      flag: true
+    })
   })
 })
